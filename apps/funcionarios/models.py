ccf02f1f82e2643dd48d5d34b0b17df6436bb4d3
RaaschDev <devraasch@gmail.com>
2022-02-21 01:56:26 -0300

    create models relations

--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from apps.departamentos.models import Departamento
+from apps.empresas.models import Empresa
 
 class Funcionario(models.Model):
     nome = models.CharField('Nome', max_length=100, help_text='Nome do funcionario')
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    departamentos = models.ManyToManyField(Departamento)
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
 
     def __str__(self) -> str:
         return self.nome
